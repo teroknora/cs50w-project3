41958d0207fef414d2fefb7ed81d716f8b9a764c
teroknora <teroknora@gmail.com>
2021-02-17 16:47:43 -0600

    emails are listed in mailboxes

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -5,7 +5,23 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
-  document.querySelector('#compose-form').addEventListener('submit', send_mail);
+  document.querySelector('#compose-form').onsubmit = () => { 
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+          recipients: document.querySelector('#compose-recipients').value,
+          subject: document.querySelector('#compose-subject').value,
+          body: document.querySelector('#compose-body').value
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+        // Print result
+        console.log(result);
+        return false;
+    });
+    return load_mailbox('sent');
+  };
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -33,23 +49,47 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  let mail = document.createElement('div');
+  mail.setAttribute('class', 'list-group');
+  mail.setAttribute('id', 'mailbox');
+  document.querySelector('#emails-view').append(mail);
+
+  // Show emails
+  fetch(`emails/${mailbox}`)
+  .then (response => response.json())
+  .then( emails => listEmails(emails))
+
 }
 
-// Send mail
-function send_mail(){
-  fetch('/emails', {
-    method: 'POST',
-    body: JSON.stringify({
-        recipients: document.querySelector('#compose-recipients').value,
-        subject: document.querySelector('#compose-subject').value,
-        body: document.querySelector('#compose-body').value
-    })
-  })
-  .then(response => response.json())
-  .then(result => {
-      // Print result
-      console.log(result);
-      return false;
-  });
+
+// Display emails in mailbox
+function listEmails(emails) {
   
- };
+  console.log(emails)
+
+  for (let i = 0; i < emails.length; i++) {
+    let info = [emails[i].sender, emails[i].subject, emails[i].timestamp]; 
+    let message = document.createElement('a');
+    message.setAttribute('href', '#');
+    message.setAttribute('class', 'd-flex list-group-item message list-group-item-action');
+    
+    
+    for (let j = 0; j < info.length; j++) {
+      let infoItem = document.createElement('div');
+      infoItem.setAttribute('class', `title${j}`);
+      infoItem.innerHTML = info[j];
+      message.append(infoItem);
+    }
+    document.querySelector('#mailbox').append(message);
+  }
+  let heading = document.createElement('div');
+  heading.setAttribute('class', 'd-flex list-group-item justify-content-between message list-group-item-action list-group-item-primary');
+  heading.innerHTML = "<div>Sender</div> <div>Subject</div> <div>Datetime Sent</div>";
+  
+  document.querySelector('#mailbox').prepend(heading);
+  
+}
+
+ 
+     
+ 
